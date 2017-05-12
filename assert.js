e31e55d71e8202bc307c7ab1f28a218fbef4e6f0
liyanyan19940914 <921258351@qq.com>
2017-05-12 14:31:41 +0800

    assert deepEqual

---
+++ assert.js
@@ -0,0 +1,27 @@
+const assert = require('assert');
+
+const obj1 = {
+    a:{
+        b:1
+    }
+};
+
+const obj2 = {
+    a:{
+        b:2
+    }
+};
+
+const obj3 = {
+    a:{
+        b:1
+    }
+};
+
+const obj4 = Object.create(obj1);
+
+//assert.deepEqual(obj1,obj2);  ////AssertionError
+
+assert.deepEqual(obj1,obj3);
+
+//assert.deepEqual(obj1,obj4);  //AssertionError: { a: { b: 1 } } deepEqual {}
