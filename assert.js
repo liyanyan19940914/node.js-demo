2e3fce3e6e40ab969abec1ba89f0004218e4bbaf
liyanyan19940914 <921258351@qq.com>
2017-05-12 14:40:04 +0800

    compare '=='and '===' different

--- assert.js
+++ assert.js
@@ -25,3 +25,7 @@ const obj4 = Object.create(obj1);
 assert.deepEqual(obj1,obj3);
 
 //assert.deepEqual(obj1,obj4);  //AssertionError: { a: { b: 1 } } deepEqual {}
+
+assert.deepEqual({a:1},{a:'1'});   //相当于‘==’
+
+assert.deepStrictEqual({a:1},{a:'1'});  //想当于‘===’，AssertionError: { a: { b: 1 } } deepEqual {}
